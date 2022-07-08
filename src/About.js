dc387cb8fdf855f7c203c099f6caa2cc7afb37b3
Caleb Rider <donutclubnft@gmail.com>
2022-07-08 11:00:02 -0400

    added skill links and tweaked about paragraph

--- src/About.js
+++ src/About.js
@@ -7,13 +7,15 @@ export default function About() {
             <div className="container-content">
                 <div className="about-card" data-aos="fade-right">
                     <h3>NFT Donut</h3>
-                    <p>Hello, and welcome! After leaving my full time position as an electrical/software engineer in 2021, 
+                    <p>&emsp;&emsp;Hello, and welcome! After leaving my full time position as an electrical/software engineer in 2021, 
                         I was sucked into the rabbit hole of Web3... I was fascinated by blockchain technology and 
-                        its massive potential! Initially, I was exploring the world of DeFi (particularly flash loans + 
+                        its massive potential! Initially, I explored the world of DeFi (particularly flash loans + 
                         exchange arbitrage), but as my desire 
-                        to learn continued to grow, I ended up settling in the NFT space as an educator and project 
-                        consultant. Now that I have a deeper understanding of the Web3 space, I'm doubling down on 
-                        the software development side of things. I'm always looking to expand my network, so if you're 
+                        to learn continued to grow, I ended up getting traction in the NFT space as an educator and project 
+                        consultant. Now that I have a broader understanding of the Web3 space, I'm doubling down on 
+                        the software development side of things, although the more I learn, the more I realize how much I don't know.
+                        <br></br>
+                        &emsp;&emsp;I'm always looking to expand my network, so if you're 
                         interested in connecting with me for any reason, feel free to reach out!
                     </p>
                     <button className="gradient-button">
--- src/Skills.js
+++ src/Skills.js
@@ -7,45 +7,45 @@ export default function Skills() {
             <div className="container-content">
                 <div className="skill-card" data-aos="fade-left">
                     <h3>Solidity</h3>
-                    <img src={require("./images/solidity-logo.png")} className="solidity-img"></img>
-                    <button className="gradient-button">
+                    <img src={require("./images/solidity-logo.png")} alt="Solidity logo" className="solidity-img"></img>
+                    <a className="gradient-button" href="https://soliditylang.org/">
                         <h4>Visit Website</h4>
-                    </button>
+                    </a>
                 </div>
                 <div className="skill-card" data-aos="fade-left">
                     <h3>React JS</h3>
-                    <img src={require("./images/react-logo.png")} className="card-img"></img>
-                    <button className="gradient-button">
+                    <img src={require("./images/react-logo.png")} alt="React JS logo" className="card-img"></img>
+                    <a className="gradient-button" href="https://reactjs.org/">
                         <h4>Visit Website</h4>
-                    </button>
+                    </a>
                 </div>
                 <div className="skill-card" data-aos="fade-left">
                     <h3>Ethers.js</h3>
-                    <img src={require("./images/ethers-js-logo.png")} className="ethers-js-img"></img>
-                    <button className="gradient-button">
+                    <img src={require("./images/ethers-js-logo.png")} alt="Ethers.js logo" className="ethers-js-img"></img>
+                    <a className="gradient-button" href="https://docs.ethers.io/v5/">
                         <h4>Visit Website</h4>
-                    </button>
+                    </a>
                 </div>
                 <div className="skill-card" data-aos="fade-left">
                     <h3>Hardhat</h3>
-                    <img src={require("./images/hardhat-logo.png")} className="card-img"></img>
-                    <button className="gradient-button">
+                    <img src={require("./images/hardhat-logo.png")} alt="Hardhat logo" className="card-img"></img>
+                    <a className="gradient-button" href="https://hardhat.org/">
                         <h4>Visit Website</h4>
-                    </button>
+                    </a>
                 </div>
                 <div className="skill-card" data-aos="fade-left">
                     <h3>Alchemy</h3>
-                    <img src={require("./images/alchemy-logo.png")} className="card-img"></img>
-                    <button className="gradient-button">
+                    <img src={require("./images/alchemy-logo.png")} alt="Alchemy logo" className="card-img"></img>
+                    <a className="gradient-button" href="https://www.alchemy.com/">
                         <h4>Visit Website</h4>
-                    </button>
+                    </a>
                 </div>
                 <div className="skill-card" data-aos="fade-left">
                     <h3>MetaMask</h3>
-                    <img src={require("./images/metamask-logo.png")} className="card-img"></img>
-                    <button className="gradient-button">
+                    <img src={require("./images/metamask-logo.png")} alt="MetaMask logo" className="card-img"></img>
+                    <a className="gradient-button" href="https://metamask.io/">
                         <h4>Visit Website</h4>
-                    </button>
+                    </a>
                 </div>
             </div>
         </div>
