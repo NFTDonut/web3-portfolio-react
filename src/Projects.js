c55fd11080d6e5b0bd94fb91024fc3b44cce4d3b
Caleb Rider <donutclubnft@gmail.com>
2022-07-12 16:45:39 -0400

    project card and button tweaks

--- src/Projects.js
+++ src/Projects.js
@@ -6,38 +6,15 @@ export default function Projects() {
             </div>
             <div className="container-content">
                 <div className="project-card" data-aos="fade-right">
-                    <h3>ERC721 Minting Bot</h3>
-                    <img src={require("./images/nft-minting-bot.PNG")} alt="Bot screenshot" className="card-img"></img>
-                    
-                    <div className="project-skills-container">
-                        <h5>Skills Required:</h5>
-                        <ul>
-                            <li>Solidity for ERC721 NFT deployment</li>
-                            <li>Hardhat for contract testing</li>
-                            <li>Alchemy for API connection</li>
-                            <li>Ethers.js for MetaMask integration</li>
-                            <li>React JS for front end development</li>
-                        </ul>
-                    </div>
-                    <div className="projectBtnContainer">
-                        <a className="gradient-button" href="https://bot.web3dev.dev/">
-                            <h4>Site</h4>
-                        </a>
-                        <a className="gradient-button" href="https://github.com/NFTDonut/nft-minting-bot">
-                            <h4>Code</h4>
-                        </a>
-                    </div>
-                </div>
-                <div className="project-card" data-aos="fade-right">
                     <h3>Web3 Dashboard</h3>
-                    <img src={require("./images/project0.PNG")} alt="Dashboard screenshot" className="card-img"></img>
+                    <img src={require("./images/web3-dashboard.PNG")} alt="Dashboard screenshot" className="card-img"></img>
                     
                     <div className="project-skills-container">
-                        <h5>Skills Required:</h5>
+                        <h5>Requirements:</h5>
                         <ul>
-                            <li>React JS for front end development</li>
-                            <li>Ethers.js for MetaMask integration</li>
-                            <li>Alchemy for API connection</li>
+                            <li><b>Next.js</b> for front end development</li>
+                            <li><b>Ethers.js</b> for MetaMask + blockchain interactions</li>
+                            <li><b>Alchemy</b> + <b>Etherscan APIs</b> to get data</li>
                         </ul>
                     </div>
                     <div className="projectBtnContainer">
@@ -50,26 +27,49 @@ export default function Projects() {
                     </div>
                 </div>
                 <div className="project-card" data-aos="fade-right">
-                    <h3>Buy Me a Donut</h3>
-                    <img src={require("./images/project0.PNG")} alt="Project screenshot" className="card-img"></img>
+                    <h3>ERC721 Minting Bot</h3>
+                    <img src={require("./images/nft-minting-bot.PNG")} alt="Bot screenshot" className="card-img"></img>
                     
                     <div className="project-skills-container">
-                        <h5>Skills Required:</h5>
+                        <h5>Requirements:</h5>
                         <ul>
-                            <li>Solidity for smart contract development</li>
-                            <li>Hardhat for testing</li>
-                            <li>Alchemy for API connection</li>
-                            <li>Ethers.js for MetaMask integration</li>
-                            <li>Next.js for front end development</li>
+                            <li><b>Solidity</b> for ERC721 NFT contract</li>
+                            <li><b>Hardhat</b> for contract testing</li>
+                            <li><b>Alchemy</b> for API connection</li>
+                            <li><b>Ethers.js</b> for MetaMask + blockchain</li>
+                            <li><b>React JS</b> for front end development</li>
                         </ul>
                     </div>
                     <div className="projectBtnContainer">
-                        <button className="gradient-button">
+                        <a className="gradient-button" href="https://bot.web3dev.dev/">
                             <h4>Site</h4>
-                        </button>
-                        <button className="gradient-button">
+                        </a>
+                        <a className="gradient-button" href="https://github.com/NFTDonut/nft-minting-bot">
                             <h4>Code</h4>
-                        </button>
+                        </a>
+                    </div>
+                </div>
+                <div className="project-card" data-aos="fade-right">
+                    <h3>Buy Me a Donut</h3>
+                    <img src={require("./images/buy-me-a-donut.PNG")} alt="Project screenshot" className="card-img"></img>
+                    
+                    <div className="project-skills-container">
+                        <h5>Requirements:</h5>
+                        <ul>
+                            <li><b>Solidity</b> for smart contract development</li>
+                            <li><b>Hardhat</b> for contract testing</li>
+                            <li><b>Alchemy</b> for API connection</li>
+                            <li><b>Ethers.js</b> for MetaMask + blockchain interactions</li>
+                            <li><b>Next.js</b> for front end development</li>
+                        </ul>
+                    </div>
+                    <div className="projectBtnContainer">
+                        <a className="gradient-button" href="https://donut.web3dev.dev/">
+                            <h4>Site</h4>
+                        </a>
+                        <a className="gradient-button" href="https://github.com/NFTDonut/buy-me-a-donut">
+                            <h4>Code</h4>
+                        </a>
                     </div>
                 </div>
             </div>
